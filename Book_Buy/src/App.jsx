57ac8ce529f68d2a27e4a9143b773726fa8945a9
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-01-30 15:04:58 +0530

    CA5-Done

--- Book_Buy/src/App.jsx
+++ Book_Buy/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-// import Register from "./Components/Register"
-import Searchpage from "./Components/SearchPage"
+import "./App.css";
+import Register from "./Components/Register";
+import Searchpage from "./Components/SearchPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Searchpage/>
-    {/* <Register/> */}
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Searchpage/>} />
+        <Route path="/Register" element={<Register/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- Book_Buy/src/Components/SearchPage.jsx
+++ Book_Buy/src/Components/SearchPage.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Components/SearchPage.css";
+import { Link } from "react-router-dom";
 
 const SearchPage = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  // we used useEffect hook to handle the errors as when the API throws error so we can handle it using the useEffect hook. 
   useEffect(() => {
+    // fetching the desired things from the API using AXIOS.  
     const fetchBooks = async () => {
       try {
         const response = await axios.get(
@@ -19,7 +22,7 @@ const SearchPage = () => {
         );
         setBooks(response.data.books);
       } catch (error) {
-        console.error("Error fetching books:", error);
+        console.error("Error fetching books", error);
       }
     };
 
@@ -29,7 +32,7 @@ const SearchPage = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
+// we are converting everything to lowerCase for us as thought the user enters capital letters or anything we can read it in lowerCase.
   const filteredBooks = books.filter((book) =>
     book.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -50,17 +53,20 @@ const SearchPage = () => {
           />
         </div>
         <div className="register">
+          {/* Routing so we can switch to different react page.*/}
+          <Link to ={"/Register"}>
           <button className="registerButton">REGISTER</button>
+          </Link>
         </div>
       </div>
       <div className="bookShelf">
         <div className="BookList">
           {filteredBooks.map((book) => (
             <div className="Book" key={book.id}>
-              <img src={book.imageLinks.thumbnail} alt={book.title} />
+              <img src={book.imageLinks.thumbnail} alt={book.title}/>
               <div className="name">{book.title}</div>
-              <div>Rating: {book.averageRating}</div>
-              <div>Price: {book.price}</div>
+              <div>Rating: {book.averageRating || "NA"}</div>
+              <div>Price: Free</div>
             </div>
           ))}
         </div>
